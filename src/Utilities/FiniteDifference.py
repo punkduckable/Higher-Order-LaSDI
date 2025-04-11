4b20919746698337cade250d3cc3603fb9ccfb40
Robert Stephany <stephany1@llnl.gov>
2025-04-11 16:45:34 -0700

    Implemented second derivative non-uniform finite difference scheme.
    I also wrote up the derivation in DeriveFiniteDifference.ipynb and wrote some tests in FiniteDifferences.ipynb. Everything appears to work well. :)

--- src/Utilities/FiniteDifference.py
+++ src/Utilities/FiniteDifference.py
@@ -102,8 +102,8 @@ def Derivative1_Order2(X : torch.Tensor, h : float) -> torch.Tensor:
 
     We use the following finite difference techniques to compute the derivative (see 
     "DeriveFiniteDifference.ipynb" for a derivation).
-        f'(x)   = (1/h)[                - (3/2)f(x) + 2f(x + h)         - (1/2)f(x + 2h) ]  + O(h^2)
-        f'(x)   = (1/h)[ (1/2)f(x - h)               + (1/2)f(x + h)    ]                   + O(h^2)
+        f'(x)   = (1/h)[                - (3/2)f(x) +   (2)f(x + h) - (1/2)f(x + 2h) ]  + O(h^2)
+        f'(x)   = (1/h)[ (1/2)f(x - h)              + (1/2)f(x + h)                  ]  + O(h^2)
 
 
     -----------------------------------------------------------------------------------------------
@@ -157,8 +157,8 @@ def Derivative1_Order4(X : torch.Tensor, h : float) -> torch.Tensor:
     We use the following finite difference techniques to compute the derivative (see 
     "DeriveFiniteDifference.ipynb" for a derivation).
         f'(x) = (1/h)[                                  - (25/12)f(x)   + (4)f(x + h)   - (3)f(x + 2h)      + (4/3)f(x + 3h)    - (1/4)f(x + 4h)]   + O(h^4)
-        f'(x) = (1/h)[                  -(1/4)f(x - h)  - (5/6)f(x)     + (2/3)f(x + h) - (1/12)f(x + 2h) ]                                         + O(h^4)
-        f'(x) = (1/h)[ (1/12)f(x - 2h)  - (2/3)f(x - h)                 + (2/3)f(x + h) - (1/12)f(x + 2h)]                                          + O(h^4)
+        f'(x) = (1/h)[                  -(1/4)f(x - h)  - (5/6)f(x)     + (2/3)f(x + h) - (1/12)f(x + 2h)                                       ]   + O(h^4)
+        f'(x) = (1/h)[ (1/12)f(x - 2h)  - (2/3)f(x - h)                 + (2/3)f(x + h) - (1/12)f(x + 2h)                                       ]   + O(h^4)
     
     -----------------------------------------------------------------------------------------------
     Arguments
@@ -204,6 +204,131 @@ def Derivative1_Order4(X : torch.Tensor, h : float) -> torch.Tensor:
 
 
 
+def Derivative2_Order2_NonUniform(X : torch.Tensor, t_Grid : torch.Tensor) -> torch.Tensor:
+    """
+    This function finds an O(h^2) approximation of the second time derivative to the time series 
+    stored in the rows of X. Specifically, we assume the i'th row of X represents a sample of a 
+    function, x, at time t_0 + i*h. We return a new tensor whose i'th row holds an O(h^2) 
+    approximation of (d^2/dt^2)x(t_0 + i*h)
+    
+    We use the following finite difference techniques to compute the derivative (see 
+    "DeriveFiniteDifference.ipynb" for a derivation).
+        f''(x) = (1/h^2)[                                                 ([-2(3a + 2b + c)]/[a(a +b)(a + b + c)])                 f(x) - ([-2(2a + 2b + c)]/[ab(b + c)])  f(x + a) + ([2(2a + b + c)]/[ab(b + c)])       f(x + a + b)   - ([2(2a + b)]/[(a + b + c)(b + c)c])f(x + a + b + c)  ] + O(h^2)
+        f''(x) = (1/h^2)[ ([2(2b + c)]/[a(a + b)(a + b + c)]) f(x - a)  - ([2(b(a + 2b + 3c) + c^2 - a^2)]/[ba(b + c)(a + b + c)]) f(x) +     ([2(b + c - a)]/[bc(a + b)]) f(x + b) + ([-2(b - a)]/[c(b + c)(a + b + c)]) f(x + b + c)                                                          ] + O(h^2)
+    
+
+    -----------------------------------------------------------------------------------------------
+    Arguments
+    -----------------------------------------------------------------------------------------------
+
+    X: A torch.Tensor object representing a time sequence. We assume that X has shape [Nt, ...] 
+    and that X[i, ...] represents the value of some function at the i'th time step. Specifically, 
+    we assume that X[i, ...] is the value of some function, x, at time t_0 + t_Grid[i].
+
+    t_Grid: A 1d torch.Tensor of shape Nt. The i'th element of this array should hold the time 
+    of the i'th time step.
+
+
+    
+    -----------------------------------------------------------------------------------------------
+    Returns
+    -----------------------------------------------------------------------------------------------
+
+    A torch.Tensor object with the same shape as X whose i'th row holds an O(h^2) approximation of 
+    the time derivative of x at the i'th time step (that is, it approximates x''(t_Grid[i])).
+    """
+
+    # For this scheme to work, X must contain at least 4 rows.
+    assert(X.shape[0] >= 4);
+
+    # Initialize a tensor to hold the time derivative.
+    d2X_dt2 : torch.Tensor  = torch.empty_like(X);
+    Nt      : int           = X.shape[0];
+
+
+    # Compute the derivative for the first time step.
+    #   f''(x) = c0 f(x) + c1 f(x + a) + c2 f(x + a + b) + c3 f(x + a + b + c)
+    #   c0 =  2(3a + 2b + c) / (a(a + b)(a + b + c))
+    #   c1 = -2(2a + 2b + c) / (ab(b + c))
+    #   c2 =  2(2a + b + c)  / (bc(a + b))
+    #   c3 = -2(2a + b)      / ((a + b + c)(b + c)c)
+    a   : float     = t_Grid[1] - t_Grid[0];
+    b   : float     = t_Grid[2] - t_Grid[1];
+    c   : float     = t_Grid[3] - t_Grid[2];
+
+    c0  : float     =  2*(3*a + 2*b + c)    / ((a*(a + b)*(a + b + c)));
+    c1  : float     = -2*(2*a + 2*b + c)    / (a*b*(b + c));
+    c2  : float     =  2*(2*a +   b + c)    / (b*c*(a + b));
+    c3  : float     = -2*(2*a +   b)        / ((a + b + c)*(b + c)*c);
+
+    d2X_dt2[0, ...] = c0*X[0, ...] + c1*X[1, ...] + c2*X[2, ...] + c3*X[3, ...];
+    
+
+    # Compute the derivative for all but the last two time steps.
+    #   f''(x) = c{-1} f(x - a) + c0 f(x) + c1 f(x + b) + c2 f(x + b + c)
+    #   c{-1}   =  2(2b + c)                        / (a(a + b)(a + b + c))
+    #   c0      =  2(b(a + 2b + 3c) + c^2 - a^2))   / (ba(b + c)(a + b + c))
+    #   c1      =  2(b + c - a)                     / (bc(a + b))
+    #   c2      = -2(b - a)                         / (c(b + c)(a + b + c))
+    a       : numpy.ndarray = t_Grid[1:(Nt - 2)]    - t_Grid[0:(Nt - 3)];
+    b       : numpy.ndarray = t_Grid[2:(Nt - 1)]    - t_Grid[1:(Nt - 2)];
+    c       : numpy.ndarray = t_Grid[3:Nt]          - t_Grid[2:(Nt - 1)];
+
+    a_b     : numpy.ndarray = a + b;
+    b_c     : numpy.ndarray = b + c;
+    a_b_c   : numpy.ndarray = a_b + c;
+    bb_bc   : numpy.ndarray = numpy.multiply(b, b_c);
+
+    cm1 : float     = torch.divide( 2*(2*b + c), torch.multiply(a, torch.multiply(a_b, a_b_c)));
+    c0  : float     = torch.divide(-2*(torch.multiply(b, (a + 2*b + 3*c)) + torch.multiply(c, c) - torch.multiply(a, a)), torch.multiply(a, torch.multiply(bb_bc,   a_b_c)));
+    c1  : float     = torch.divide( 2*(b_c - a), torch.multiply(b, torch.multiply(c, a_b)));
+    c2  : float     = torch.divide(-2*(b - a),  torch.multiply(c, torch.multiply(b_c, a_b_c)));
+
+    cm1 = cm1.reshape([-1] + [1]*(len(X.shape) - 1));
+    c0  = c0.reshape( [-1] + [1]*(len(X.shape) - 1));
+    c1  = c1.reshape( [-1] + [1]*(len(X.shape) - 1));
+    c2  = c2.reshape( [-1] + [1]*(len(X.shape) - 1));
+
+    d2X_dt2[1:(Nt - 2), ...] = cm1*X[0:(Nt - 3), ...] + c0*X[1:(Nt - 2), ...] + c1*X[2:(Nt - 1), ...] + c2*X[3:(Nt), ...];
+
+
+    # Compute the derivative for the second to last time step
+    #   f''(x) = c{-1} f(x + a) + c{-2} f(x) + c{-3} f(x - b) + c{-4} f(x - b - c)
+    #   c{-1}   =  2(2b + c)                        / (a(a + b)(a + b + c))
+    #   c{-2}   =  2(b(a + 2b + 3c) + c^2 - a^2))   / (ba(b + c)(a + b + c))
+    #   c{-3}   =  2(b + c - a)                     / (bc(a + b))
+    #   c{-4}   = -2(b - a)                         / (c(b + c)(a + b + c))
+    c       : float = t_Grid[-3] - t_Grid[-4];
+    b       : float = t_Grid[-2] - t_Grid[-3];
+    a       : float = t_Grid[-1] - t_Grid[-2];
+
+    cm1     : float = ( 2*(2*b + c))                     / (a*(a + b)*(a + b + c));
+    cm2     : float = (-2*b*(a + 2*b + 3*c) + c*c - a*a) / (a*b*(b + c)*(a + b + c));
+    cm3     : float = ( 2*(b + c - a))                   / (b*c*(a + b));
+    cm4     : float = (-2*(b - a))                       / (c*(b + c)*(a + b + c));
+
+    d2X_dt2[-2, ...]    = cm4*X[-4, ...] + cm3*X[-3, ...] + cm2*X[-2, ...] + cm1*X[-1, ...];
+
+
+    # Compute the derivative for the final time step.
+    #   f''(x) = c{-1} f(x) + c{-2} f(x - a) + c{-3} f(x - a - b) + c{-4} f(x - a - b - c)
+    #   c{-1} =  2(3a + 2b + c) / (a(a + b)(a + b + c))
+    #   c{-2} = -2(2a + 2b + c) / (ab(b + c))
+    #   c{-3} =  2(2a + b + c)  / (ab(b + c))
+    #   c{-4} = -2(2a + b)      / ((a + b + c)(b + c)c)
+
+    cm1     : float =  2*(3*a + 2*b + c)    / ((a*(a + b)*(a + b + c)));
+    cm2     : float = -2*(2*a + 2*b + c)    / (a*b*(b + c));
+    cm3     : float =  2*(2*a +   b + c)    / (b*c*(a + b));
+    cm4     : float = -2*(2*a +   b)        / ((a + b + c)*(b + c)*c);
+
+    d2X_dt2[-1, ...] = cm1*X[-1, ...] + cm2*X[-2, ...] + cm3*X[-3, ...] + cm4*X[-4, ...];
+
+    # All done!
+    return d2X_dt2;
+
+
+
 def Derivative2_Order2(X : torch.Tensor, h : float) -> torch.Tensor:
     """
     This function finds an O(h^2) approximation of the second time derivative to the time series 
@@ -213,8 +338,8 @@ def Derivative2_Order2(X : torch.Tensor, h : float) -> torch.Tensor:
     
     We use the following finite difference techniques to compute the derivative (see 
     "DeriveFiniteDifference.ipynb" for a derivation).
-        f''(x) = (1/h^2)[             2f(x) - 5f(x + h)    + 4f(x + 2h)    - 3f(x + 3h) ] + O(h^2)
-        f''(x) = (1/h^2)[ f(x - h)  - 2f(x) + f(x + h) ]                                  + O(h^2)
+        f''(x) = (1/h^2)[             (2)f(x) -  (5)f(x + h)   + (4)f(x + 2h)   - f(x + 3h) ] + O(h^2)
+        f''(x) = (1/h^2)[ f(x - h)  - (2)f(x) +     f(x + h)                                ] + O(h^2)
     
 
     -----------------------------------------------------------------------------------------------
