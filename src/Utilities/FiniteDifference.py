Fixed bug in second order derivative for non-uniform spacing
There was a parenthesis issue in the computation for the second to last time step. I fixed it!

src/Utilities/FiniteDifference.py
@@ -265,13 +265,13 @@ def Derivative2_Order2_NonUniform(X : torch.Tensor, t_Grid : torch.Tensor) -> to
     #   f''(x) = c0 f(x) + c1 f(x + a) + c2 f(x + a + b) + c3 f(x + a + b + c)
     #   c0 =  2(3a + 2b + c) / (a(a + b)(a + b + c))
     #   c1 = -2(2a + 2b + c) / (ab(b + c))
-    #   c2 =  2(2a + b + c)  / (bc(a + b))
-    #   c3 = -2(2a + b)      / ((a + b + c)(b + c)c)
+    #   c2 =  2(2a +  b + c) / (bc(a + b))
+    #   c3 = -2(2a +  b)     / ((a + b + c)(b + c)c)
     a   : float     = t_Grid[1] - t_Grid[0];
     b   : float     = t_Grid[2] - t_Grid[1];
     c   : float     = t_Grid[3] - t_Grid[2];
 
-    c0  : float     =  2*(3*a + 2*b + c)    / ((a*(a + b)*(a + b + c)));
+    c0  : float     =  2*(3*a + 2*b + c)    / (a*(a + b)*(a + b + c));
     c1  : float     = -2*(2*a + 2*b + c)    / (a*b*(b + c));
     c2  : float     =  2*(2*a +   b + c)    / (b*c*(a + b));
     c3  : float     = -2*(2*a +   b)        / ((a + b + c)*(b + c)*c);
@@ -282,7 +282,7 @@ def Derivative2_Order2_NonUniform(X : torch.Tensor, t_Grid : torch.Tensor) -> to
     # Compute the derivative for all but the last two time steps.
     #   f''(x) = c{-1} f(x - a) + c0 f(x) + c1 f(x + b) + c2 f(x + b + c)
     #   c{-1}   =  2(2b + c)                        / (a(a + b)(a + b + c))
-    #   c0      =  2(b(a + 2b + 3c) + c^2 - a^2))   / (ba(b + c)(a + b + c))
+    #   c0      = -2(b(a + 2b + 3c) + c^2 - a^2))   / (ba(b + c)(a + b + c))
     #   c1      =  2(b + c - a)                     / (bc(a + b))
     #   c2      = -2(b - a)                         / (c(b + c)(a + b + c))
     a       : torch.Tensor = t_Grid[1:(Nt - 2)]    - t_Grid[0:(Nt - 3)];
@@ -294,10 +294,10 @@ def Derivative2_Order2_NonUniform(X : torch.Tensor, t_Grid : torch.Tensor) -> to
     a_b_c   : torch.Tensor = a_b + c;
     bb_bc   : torch.Tensor = torch.multiply(b, b_c);
 
-    cm1 : float     = torch.divide( 2*(2*b + c), torch.multiply(a, torch.multiply(a_b, a_b_c)));
+    cm1 : float     = torch.divide( 2*(2*b + c),        torch.multiply(a, torch.multiply(a_b, a_b_c)));
     c0  : float     = torch.divide(-2*(torch.multiply(b, (a + 2*b + 3*c)) + torch.multiply(c, c) - torch.multiply(a, a)), torch.multiply(a, torch.multiply(bb_bc,   a_b_c)));
-    c1  : float     = torch.divide( 2*(b_c - a), torch.multiply(b, torch.multiply(c, a_b)));
-    c2  : float     = torch.divide(-2*(b - a),  torch.multiply(c, torch.multiply(b_c, a_b_c)));
+    c1  : float     = torch.divide( 2*(b_c - a),        torch.multiply(b, torch.multiply(c, a_b)));
+    c2  : float     = torch.divide(-2*(b - a),          torch.multiply(c, torch.multiply(b_c, a_b_c)));
 
     cm1 = cm1.reshape([-1] + [1]*(len(X.shape) - 1));
     c0  = c0.reshape( [-1] + [1]*(len(X.shape) - 1));
@@ -310,17 +310,17 @@ def Derivative2_Order2_NonUniform(X : torch.Tensor, t_Grid : torch.Tensor) -> to
     # Compute the derivative for the second to last time step
     #   f''(x) = c{-1} f(x + a) + c{-2} f(x) + c{-3} f(x - b) + c{-4} f(x - b - c)
     #   c{-1}   =  2(2b + c)                        / (a(a + b)(a + b + c))
-    #   c{-2}   =  2(b(a + 2b + 3c) + c^2 - a^2))   / (ba(b + c)(a + b + c))
+    #   c{-2}   = -2(b(a + 2b + 3c) + c^2 - a^2))   / (ba(b + c)(a + b + c))
     #   c{-3}   =  2(b + c - a)                     / (bc(a + b))
     #   c{-4}   = -2(b - a)                         / (c(b + c)(a + b + c))
     c       : float = t_Grid[-3] - t_Grid[-4];
     b       : float = t_Grid[-2] - t_Grid[-3];
     a       : float = t_Grid[-1] - t_Grid[-2];
 
-    cm1     : float = ( 2*(2*b + c))                     / (a*(a + b)*(a + b + c));
-    cm2     : float = (-2*b*(a + 2*b + 3*c) + c*c - a*a) / (a*b*(b + c)*(a + b + c));
-    cm3     : float = ( 2*(b + c - a))                   / (b*c*(a + b));
-    cm4     : float = (-2*(b - a))                       / (c*(b + c)*(a + b + c));
+    cm1     : float = ( 2*(2*b + c))                        / (a*(a + b)*(a + b + c));
+    cm2     : float = (-2*(b*(a + 2*b + 3*c) + c*c - a*a))  / (a*b*(b + c)*(a + b + c));
+    cm3     : float = ( 2*(b + c - a))                      / (b*c*(a + b));
+    cm4     : float = (-2*(b - a))                          / (c*(b + c)*(a + b + c));
 
     d2X_dt2[-2, ...]    = cm4*X[-4, ...] + cm3*X[-3, ...] + cm2*X[-2, ...] + cm1*X[-1, ...];
 
@@ -329,7 +329,7 @@ def Derivative2_Order2_NonUniform(X : torch.Tensor, t_Grid : torch.Tensor) -> to
     #   f''(x) = c{-1} f(x) + c{-2} f(x - a) + c{-3} f(x - a - b) + c{-4} f(x - a - b - c)
     #   c{-1} =  2(3a + 2b + c) / (a(a + b)(a + b + c))
     #   c{-2} = -2(2a + 2b + c) / (ab(b + c))
-    #   c{-3} =  2(2a + b + c)  / (ab(b + c))
+    #   c{-3} =  2(2a + b + c)  / (bc(a + b))
     #   c{-4} = -2(2a + b)      / ((a + b + c)(b + c)c)
 
     cm1     : float =  2*(3*a + 2*b + c)    / ((a*(a + b)*(a + b + c)));
@@ -412,7 +412,7 @@ def Derivative2_Order4(X : torch.Tensor, h : float) -> torch.Tensor:
 
     We use the following finite difference techniques to compute the derivative (see 
     "DeriveFiniteDifference.ipynb" for a derivation).
-    
+
         f''(x) = (1/h^2)[                                     (15/4)f(x)    - (12 + 5/6)f(x + h)    + (17 + 5/6)f(x + 2h)   - (13)f(x + 3h)     + (5 + 1/12)f(x + 4h)   - (5/6)f(x + 5h) ]  + O(h^4)
         f''(x) = (1/h^2)[                     (5/6)f(x - h) - (5/4)f(x)     - (1/3)f(x + h)         + (7/6)f(x + 2h)        - (1/2)f(x + 3h)    + (1/12)f(x + 4h)]                          + O(h^4)
         f''(x) = (1/h^2)[ -(1/12)f(x - 2h)  + (4/3)f(x - h) - (5/2)f(x)     + (4/3)f(x + h)         - (1/12)f(x + 2h)]                                                                      + O(h^4)