7de9c1ed2cd17a48a39065bd1b22dcae912b4c44
Robert Stephany <stephany1@llnl.gov>
2025-06-04 16:10:20 -0700

    Added WaveEquation class
    This is a driver class for the new WaveEquation example. I still need to test it. I also added the periodic hexagon mesh for the updated advection example.

---
+++ src/Physics/WaveEquation.py
@@ -0,0 +1,176 @@
+# -------------------------------------------------------------------------------------------------
+# Imports and Setup
+# -------------------------------------------------------------------------------------------------
+
+
+# Add the main directory to the search path.
+import logging;
+import  os;
+import  sys;
+PyMFEM_Path     : str   = os.path.abspath(os.path.join(os.path.dirname(__file__), "PyMFEM"));
+sys.path.append(PyMFEM_Path);
+
+import  numpy;
+from    scipy.special                   import  erfc;
+import  torch;
+
+from    Physics                         import  Physics;
+from    wave_equation                   import  Simulate;
+
+
+LOGGER : logging.Logger = logging.getLogger(__name__);
+
+
+
+
+# -------------------------------------------------------------------------------------------------
+# WaveEquation class
+# -------------------------------------------------------------------------------------------------
+
+class WaveEquation(Physics):
+    def __init__(self, config : dict, param_names : list[str] = None) -> None:
+        """
+        Initialize a WaveEquation object. This class acts as a wrapper around the MFEM-based solver 
+        implemented in ``wave_equation.py`` within the ``PyMFEM`` sub-directory. The solver models 
+        the propagation of a wave in a two dimensional domain.
+
+        
+        -------------------------------------------------------------------------------------------
+        Arguments
+        -------------------------------------------------------------------------------------------
+
+        config : dict
+            Dictionary housing the settings for the WaveEquation object. This should be the 
+            ``physics`` sub-dictionary of the configuration file.
+        
+        param_names : list[str], optional
+            Names of parameters appearing in the initial condition. The wave equation example has 
+            no parameters so this should be an empty list.
+
+        
+        
+        -------------------------------------------------------------------------------------------
+        Returns
+        -------------------------------------------------------------------------------------------
+        
+        None
+        """
+
+        # Run checks
+        assert(isinstance(param_names, list));
+        assert(len(param_names) == 2);
+        assert('c' in param_names);
+        assert('k' in param_names);
+        assert('WaveEquation' in config);
+
+        # Call the super class initializer.
+        super().__init__(config         = config,
+                         param_names    = param_names,
+                         Uniform_t_Grid = False);
+
+        # Run a short simulation to determine the frame shape and positions.
+        U, DtU, X, T                        = Simulate(t_final = 0);
+        self.Frame_Shape    : list[int]     = list(U.shape[1:]);
+        self.X_Positions    : numpy.ndarray = numpy.copy(X);            # shape = (2, N)    
+        LOGGER.debug("Frame shape: %s" % str(self.Frame_Shape));
+
+        # Since there are two spatial dimensions, set spatial_dim accordingly.
+        self.spatial_dim    : int           = 2;
+        self.n_IC           : int           = 2;
+
+        # Determine which index corresponds to c (wave speed) and which to k (decay rate in the IC).
+        self.c_idx  : int   = self.param_names.index('c');
+        self.k_idx  : int   = self.param_names.index('k');        
+        return;
+
+
+
+    def initial_condition(self, param : numpy.ndarray) -> list[numpy.ndarray]:
+        """
+        Evaluate the initial condition of the advection equation at the positions stored in 
+        ``self.X_Positions``. For the default problem considered in the MFEM example, the initial 
+        state is defined by
+             
+            u_0(x, t) = exp(-k * |x|^2)
+
+        Here, k = param[0] and c = param[1]. 
+        
+        Note 1: c is unused in the IC but defines the wave speed in wave equation, 
+        
+                d^2u/dt^2 = c^2 * d^2u/dx^2,
+       
+        Note 2: The initial condition is defined on a star-shaped domain.
+        
+
+        -------------------------------------------------------------------------------------------
+        Arguments
+        -------------------------------------------------------------------------------------------
+
+        param : numpy.ndarray, shape = (self.n_p)
+            A two element array holding the values of the k and c parameters.
+
+
+                
+        -------------------------------------------------------------------------------------------
+        Returns
+        -------------------------------------------------------------------------------------------
+
+        u0 : list[numpy.ndarray], len = 2   
+            A two element list whose i'th element is a numpy.ndarray of shape (1, N) holding the 
+            value of the i'th time derivative of the initial condition at each of the N spatial 
+            locations. 
+        """
+
+        # Checks
+        assert(isinstance(param, numpy.ndarray));
+        assert(len(param.shape) == 1);
+        assert(param.shape[0]   == self.n_p);
+        assert(self.X_Positions is not None);
+
+        # Evaluate the initial condition.
+        norm2 : float           = numpy.sum(numpy.square(self.X_Positions), axis = 0);
+        u0    : numpy.ndarray   = numpy.exp(-norm2*param[self.k_idx]);
+        
+        return [u0];
+
+
+
+    def solve(self, param : numpy.ndarray) -> tuple[list[torch.Tensor], torch.Tensor]:
+        """
+        Solve the advection equation. This function simply calls the MFEM solver ``Simulate`` and 
+        packages its output so that it conforms to the :class:`Physics` API.
+
+
+        -------------------------------------------------------------------------------------------
+        Arguments
+        -------------------------------------------------------------------------------------------
+
+        param : numpy.ndarray, shape = (self.n_p)
+            A two element array holding the values of the k and w parameters.
+
+
+        -------------------------------------------------------------------------------------------
+        Returns
+        -------------------------------------------------------------------------------------------
+
+        X, t_grid 
+
+        X : list[torch.Tensor], len = 2 
+            A two element list containing a tensor of shape (n_t, *self.Frame_Shape) with the 
+            solution trajectory. The first element is the solution u(x, t) and the second element 
+            is the solution du/dt(x, t).
+
+        t_grid : torch.Tensor, shape = (n_t)
+            A one dimensional tensor of the corresponding times.
+        """
+
+        assert(isinstance(param, numpy.ndarray));
+        assert(len(param.shape) == 1);
+        assert(param.shape[0]   == self.n_p);
+
+        # Solve the PDE using the external MFEM script.
+        U, DtU, _, Times = Simulate(k = param[self.k_idx], c = param[self.c_idx], VisIt = False);
+
+        X       : list[torch.Tensor] = [torch.Tensor(U), torch.Tensor(DtU)];
+        t_Grid  : torch.Tensor       = torch.Tensor(Times);
+        return X, t_Grid;
